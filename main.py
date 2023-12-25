c75c611b9cc8706d2d14b6244d104a768c108552
ozzy <businessozzy27@gmail.com>
2023-12-25 14:04:55 -0500

    stuff

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygetwindow as gw
 import psutil
 import win32process
+import lyricsgenius as lg
+import re
 
 spotify_process_name = "Spotify.exe"
+
 windows = gw.getWindowsWithTitle("")
 process_names = []
+
 for window in windows:
     try:
         process_id = win32process.GetWindowThreadProcessId(window._hWnd)[1]
@@ -12,12 +16,18 @@ for window in windows:
         process_names.append(process_name)
     except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
         process_names.append("(not available)")
-print("Process Names:", process_names)
-song = next((window for window, process_name in zip(windows, process_names) if process_name == spotify_process_name), None)
-if not song:
+
+song_window = next((window for window, process_name in zip(windows, process_names) if process_name == spotify_process_name), None)
+
+if not song_window:
     print("Spotify isn't running!")
     exit()
 
+genius = lg.Genius("THIS API KEY IS NOT REQUIRED - YOU DON'T NEED TO REPLACE THIS! :D")
 
-while song:
-    print(song.title.split(" - ")[1])
+#while song_window:
+window_title_parts = song_window.title.split(" - ")
+song_title = window_title_parts[1]
+song = genius.search_song(song_title, window_title_parts[0])
+lyrics=re.sub(r'\d+Embed$', '', song.lyrics.split(f"{song_title} Lyrics")[1]) if song else f"Lyrics to {song_title} by {window_title_parts[0]} not found!"
+print(lyrics)
